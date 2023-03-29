47873ccf154dbd9c01b09e8318ddb97b4ce1bc72
Maruthi2896 <maruthikj4@gmail.com>
2023-03-29 15:03:55 +0530

    fine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import useState from 'react';
 
 function App() {
+  const [like,setLike]=useState(0);
   return (
     <div className="App">
-      
+      <div>
+<button onClick={()=>setLike(like+1)}> {like} Decrement</button>
+<button  onClick={()=>setLike(like-1)}> {like} Increment</button>
+      </div>
     </div>
   );
 }
+
 
 export default App;
