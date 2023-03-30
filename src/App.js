be6cbac14a6f3cd2725998fc43fc6c24c65284d8
Maruthi2896 <maruthikj4@gmail.com>
2023-03-30 11:55:06 +0530

    corrected

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import useState from 'react';
+import {useState }from 'react';
 
 function App() {
   const [like,setLike]=useState(0);
+  const [dislike,setDislike]=useState(0);
   return (
     <div className="App">
-      <div>
-<button onClick={()=>setLike(like+1)}> {like} Decrement</button>
-<button  onClick={()=>setLike(like-1)}> {like} Increment</button>
+      <div> 
+ <button onClick={()=>setLike(like-1)}>Decrement- {like} </button>
+ <button  onClick={()=>setDislike(dislike+1)}> Increment-{dislike} </button>
       </div>
     </div>
   );
 }
-
-
+// ()=>setLike(like+1)
+// ()=>setLike(like-1)
 export default App;
